152214c13a3f1ec33ec755ded93c8bcd98204174
jaysmito <jaysmito101@gmail.com>
2024-05-06 05:43:41 +0530

    big bang

---
+++ data.js
@@ -0,0 +1,212 @@
+export const navBarLabels = [
+    {
+        label: 'Home',
+        url: '#/',
+        icon: 'fa-solid fa-home',
+        default: true
+    },
+    {
+        label: 'About',
+        url: '#/about',
+        icon: 'fa-solid fa-user'
+    },
+    {
+        label: 'Skills',
+        url: '#/skills',
+        icon: 'fa-solid fa-tools'
+    },
+    {
+        label: 'Projects',
+        url: '#/projects',
+        icon: 'fa-solid fa-folder'
+    },
+    {
+        label: 'Links',
+        url: '#/links',
+        icon: 'fa-solid fa-link'
+    },
+    {
+        label: 'Contact',
+        url: '#/contact',
+        icon: 'fa-solid fa-envelope'
+    },
+    {
+        label: 'Dark Mode/Light Mode',
+        url: '#/dark-mode',
+        icon: 'fa-solid fa-sun'
+    },
+    {
+        label: 'Github',
+        url: 'https://github.com/Jaysmito101',
+        icon: 'fa-brands fa-github'
+    }
+];
+
+export const links = [
+    {
+        label: 'GitHub',
+        id: 'Jaysmito101',
+        url: 'https://github.com/Jaysmito101',
+        icon: 'fa-brands fa-github'
+    },
+    {
+        label: 'Discord',
+        id: 'Jaysmito101',
+        url: 'https://discord.com/users/Jaysmito101',
+        icon: 'fa-brands fa-discord'
+    },
+    {
+        label: 'LinkedIn',
+        id: 'Jaysmito Mukherjee',
+        url: 'https://www.linkedin.com/in/jaysmito',
+        icon: 'fa-brands fa-linkedin'
+    },
+    {
+        label: 'Twitter',
+        id: 'jaysmito101',
+        url: 'https://twitter.com/jaysmito101',
+        icon: 'fa-brands fa-twitter'
+    },
+    {
+        label: 'Stack Overflow',
+        id: 'jaysmito-mukherjee',
+        url: 'https://stackoverflow.com/users/14911094/jaysmito-mukherjee',
+        icon: 'fa-brands fa-stack-overflow'
+    },
+    {
+        label: 'Hashnode',
+        id: 'jaysmito101',
+        url: 'https://jaysmito101.hashnode.dev/',
+        icon: 'fa-brands fa-hashnode'
+    },
+    {
+        label: 'Reddit',
+        id: 'Beginning-Safe4282',
+        url: 'https://new.reddit.com/user/Beginning-Safe4282',
+        icon: 'fa-brands fa-reddit'
+    },
+    {
+        label: 'crates.io',
+        id: 'Jaysmito101',
+        url: 'https://crates.io/users/Jaysmito101',
+        icon: 'fa-brands fa-rust'
+    },
+    {
+        label: 'PyPi',
+        id: 'jaysmito101',
+        url: 'https://pypi.org/user/jaysmito101/',
+        icon: 'fa-brands fa-python'
+    }
+];
+
+
+export const projects = [
+    {
+        name: 'TerraForge3D',
+        description: 'A Cross Platform Professional Procedural Terrain Generation & Texturing Tool.',
+        url: 'https://github.com/Jaysmito101/TerraForge3D',
+        image: './img/tf3d.png',
+        stars: 910,
+    },
+    {
+        name: 'CGL',
+        description: 'CGL (C Game Library) is a multipurpose library mainly for recreational coding / demo scenes / prototyping / small games / experimentation.',
+        url: 'https://github.com/Jaysmito101/cgl',
+        image: 'https://raw.githubusercontent.com/Jaysmito101/cgl/main/images/CGL_logo_low_mid_res.png',
+        stars: 387,
+    },
+    {
+        name: 'rusty.hpp',
+        description: 'A Borrow Checker and Memory Ownership System for C++20 (heavily inspired from Rust)',
+        url: 'https://github.com/Jaysmito101/rusty.hpp',
+        image: 'https://i.ibb.co/jwkwzYs/OIG3.jpg',
+        stars: 122,
+    },
+    {
+        name: 'lexpp',
+        description: 'Small Extremely Powerful Header Only C++ Lexical Analyzer/String Parser Library',
+        url: 'https://github.com/Jaysmito101/lexpp',
+        image: 'https://i.ibb.co/rG57k5b/OIG3.jpg',
+        stars: 60,
+    },
+    {
+        name: 'Xlux',
+        description: 'A Multi Threaded Software Renderer',
+        url: 'https://github.com/Jaysmito101/Xlux',
+        image: 'https://github.com/Jaysmito101/Xlux/raw/main/logo.jpg',
+        stars: 37,
+    },
+    {
+        name: 'WFCForge',
+        description: 'A Procedural + Directed Game Art Generator',
+        url: 'https://github.com/Jaysmito101/WFCForge',
+        image: 'https://github.com/Jaysmito101/WFCForge/raw/main/Images/ss3.png',
+        stars: 36,
+    },
+];
+
+
+export const skills = [
+    {
+        name: "C++",
+        progress: 95
+    },
+    {
+        name: "C",
+        progress: 95
+    },
+    {
+        name: "Rust",
+        progress: 90
+    },
+    {
+        name: "Python",
+        progress: 80
+    },
+    {
+        name: "JavaScript",
+        progress: 70
+    },
+    {
+        name: "OpenGL",
+        progress: 95
+    },
+    {
+        name: "WebGPU",
+        progress: 85
+    },
+    {
+        name: "Vulkan",
+        progress: 85
+    },
+    {
+        name: "Unity",
+        progress: 85
+    },
+    {
+        name: "PyTorch",
+        progress: 90
+    },
+    {
+        name: "Tensorflow",
+        progress: 75
+    },
+    {
+        name: "OOP",
+        progress: 85
+    },
+    {
+        name: "Game Dev",
+        progress: 85
+    },
+    
+    {
+        name: "Unreal Engine",
+        progress: 35
+    },
+    {
+        name: "Blender",
+        progress: 70
+    },
+
+];
---
+++ main.js
@@ -0,0 +1,338 @@
+
+import { links, navBarLabels, projects, skills } from './data.js';
+
+function setContent(item) {
+    document.querySelectorAll('.content').forEach(content => {
+        content.innerHTML = item.innerHTML;
+    });
+}
+
+function toggleDarkMode() {
+    const currentTheme = document.documentElement.getAttribute('data-theme') || 'dark';
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.body.classList.add('light');
+        return true;
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.body.classList.remove('light');
+        return false;
+    }
+}
+
+// setLightMode();
+
+function reSecureCallback() {
+    document.querySelectorAll('.content-contact-form').forEach(reSecure => {
+        reSecure.onsubmit = (ev) => {
+            ev.preventDefault();
+            const form = ev.target;
+            const formData = new FormData(form);
+            fetch('https://formspree.io/f/mgegddvb', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json'
+                },
+                body: formData
+            }).then(response => {
+                if (response.status === 200) {
+                    form.reset();
+                    alert('Message sent successfully');
+                } else {
+                    alert('Failed to send message');
+                }
+            });
+            
+            form.reset();
+            
+        };
+    });
+}
+
+
+
+function setPage(url) {
+    // remove all query params
+    url = url.split('?')[0];
+    if (url === '') {
+        url = 'home';
+    }
+    
+    const page_data = document.querySelector(`#raw-data`).getElementsByClassName(url)[0];
+    
+    if (page_data) {
+        setContent(page_data);
+    } else {
+        const error_page = document.querySelector('#raw-data').getElementsByClassName('error')[0];
+        document.title = "Jaysmito // 404";
+        if (error_page) {
+            setContent(error_page);
+        } else {
+            document.querySelectorAll('.content').forEach(content => {
+                content.innerHTML = "<h1>404: Page not found</h1>";
+            });
+        }
+    }
+
+    reSecureCallback();
+}
+
+function navigateTo(url) {
+
+
+    if (url === '#/dark-mode') {
+
+        const navButton = document.querySelector('.nav-button[data-url="#/dark-mode"]');
+
+        if(toggleDarkMode()) {
+            navButton.querySelector('.nav-icon').classList.remove('fa-sun');
+            navButton.querySelector('.nav-icon').classList.add('fa-moon');
+        } else {
+            navButton.querySelector('.nav-icon').classList.remove('fa-moon');
+            navButton.querySelector('.nav-icon').classList.add('fa-sun');
+        }
+        return;
+    }
+    
+    document.querySelectorAll('.nav-button').forEach(navButton => {
+        if (navButton.getAttribute('data-url') === url) {
+            navButton.classList.add('active');
+        } else {
+            navButton.classList.remove('active');
+        }
+    });
+
+
+    if (url[0] === '#') {
+        window.history.replaceState({}, '', url);
+        document.title = "Jaysmito // " + url.slice(2);
+        setPage(url.slice(2));
+    } else {
+        window.open(url, '_blank');
+    }
+}
+
+function navigateToCurrent() {
+    navigateTo(window.location.hash);
+}
+
+document.querySelectorAll('.content-links-container').forEach(contentLinksContainer => {
+    contentLinksContainer.innerHTML = "";
+    links.forEach(link => {
+        const contentLinksItem = document.createElement('div');
+        contentLinksItem.classList.add('content-links-item');
+        {
+            const contentLinksText = document.createElement('div');
+            contentLinksText.classList.add('content-links-text');
+            contentLinksText.innerHTML = link.label + (link.id ? ` <span class="content-links-id">${link.id}</span>` : '');
+            contentLinksItem.appendChild(contentLinksText);
+        }
+        {
+            const contentLinksIcon = document.createElement('div');
+            contentLinksIcon.classList.add('content-links-icon');
+            {
+                const icon = document.createElement('i');
+                icon.classList.add('fa');
+                for (const iconClass of link.icon.split(' ')) {
+                    icon.classList.add(iconClass);
+                }
+                contentLinksIcon.appendChild(icon);
+            }
+            contentLinksItem.appendChild(contentLinksIcon);
+        }
+        // wrap it up in a a
+        const linkElement = document.createElement('a');
+        linkElement.href = link.url;
+        linkElement.target = '_blank';
+        linkElement.style.width = '100%';
+        linkElement.appendChild(contentLinksItem);
+        contentLinksContainer.appendChild(linkElement);
+    });
+});
+
+document.querySelectorAll('.content-projects-container').forEach(contentProjectsContainer => {
+    contentProjectsContainer.innerHTML = "";
+    projects.forEach(project => {
+        const contentProjectsCard = document.createElement('div');
+        contentProjectsCard.classList.add('content-projects-card');
+        
+        const projectImage = document.createElement('img');
+        projectImage.src = project.image;
+        projectImage.alt = project.name;
+        projectImage.classList.add('content-projects-image');
+        contentProjectsCard.appendChild(projectImage);
+        
+        const contentProjectsContent = document.createElement('div');
+        contentProjectsContent.classList.add('content-projects-content');
+        
+        const projectTitle = document.createElement('h3');
+        projectTitle.classList.add('content-projects-title');
+        projectTitle.textContent = project.name;
+        contentProjectsContent.appendChild(projectTitle);
+
+        const projectStartCount = document.createElement('p');
+        projectStartCount.classList.add('content-projects-star-count');
+        projectStartCount.innerHTML = `<i class="fa fa-star"></i> Stars: ${project.stars}`;
+        contentProjectsContent.appendChild(projectStartCount);
+        
+        const projectDescription = document.createElement('p');
+        projectDescription.classList.add('content-projects-description');
+        projectDescription.textContent = project.description;
+        contentProjectsContent.appendChild(projectDescription);
+        
+        const projectLink = document.createElement('a');
+        projectLink.href = project.url;
+        projectLink.classList.add('content-projects-link');
+        projectLink.textContent = 'View Project';
+        contentProjectsContent.appendChild(projectLink);
+        
+        contentProjectsCard.appendChild(contentProjectsContent);
+        contentProjectsContainer.appendChild(contentProjectsCard);
+    });
+});
+
+document.querySelectorAll('.content-skills-container').forEach(contentSkillsContainer => {
+    contentSkillsContainer.innerHTML = "";
+
+    skills.forEach(skill => {
+        const contentSkillsItem = document.createElement('div');
+        contentSkillsItem.classList.add('content-skills-item');
+        
+        const skillName = document.createElement('div');
+        skillName.classList.add('content-skills-name');
+        skillName.textContent = skill.name;
+        contentSkillsItem.appendChild(skillName);
+        
+        const contentSkillsProgress = document.createElement('div');
+        contentSkillsProgress.classList.add('content-skills-progress');
+        
+        const contentSkillsProgressBar = document.createElement('div');
+        contentSkillsProgressBar.classList.add('content-skills-progress-bar');
+        contentSkillsProgressBar.style.width = `${skill.progress}%`;
+        contentSkillsProgress.appendChild(contentSkillsProgressBar);
+        
+        const contentSkillsPercent = document.createElement('div');
+        contentSkillsPercent.classList.add('content-skills-percent');
+        contentSkillsPercent.textContent = `${skill.progress}%`;
+        contentSkillsProgress.appendChild(contentSkillsPercent);
+        
+        contentSkillsItem.appendChild(contentSkillsProgress);
+        
+        contentSkillsContainer.appendChild(contentSkillsItem);
+    });
+
+});
+
+// setup nav bar
+document.querySelectorAll('.nav-bar').forEach(navBar => {
+    navBar.innerHTML = "";
+    const pieWheel = document.querySelector('.pie-wheel');
+    pieWheel.innerHTML = "";
+    navBarLabels.forEach(label => {
+        const navItem = document.createElement('div');
+        navItem.classList.add('nav-item');
+        {
+            const navButton = document.createElement('div');
+            navButton.classList.add('nav-button');
+            if (label.default) {
+                navButton.classList.add('active');
+            }
+            navButton.setAttribute('data-url', label.url);
+            navButton.onclick = (ev) => {
+                navigateTo(label.url);
+            }
+
+            {
+                const icon = document.createElement('i');
+                icon.classList.add('nav-icon');
+                label.icon.split(' ').forEach(iconClass => {
+                    icon.classList.add(iconClass);
+                });
+                navButton.appendChild(icon);
+            }
+            navItem.appendChild(navButton);
+        }
+        {
+            const tooltip = document.createElement('div');
+            tooltip.classList.add('tooltip');
+            tooltip.textContent = label.label;
+            navItem.appendChild(tooltip);
+        }
+        navBar.appendChild(navItem);
+
+
+        const pieWheelItem = document.createElement('div');
+        pieWheelItem.classList.add('pie-arc');
+        pieWheelItem.setAttribute('data-index', navBarLabels.indexOf(label));
+        pieWheelItem.setAttribute('data-label', label.label);
+        pieWheelItem.setAttribute('data-url', label.url);
+        pieWheelItem.innerHTML = `<i class="${label.icon}"></i>`;
+        pieWheel.appendChild(pieWheelItem);
+    });
+});
+
+
+
+navigateToCurrent();
+window.onpopstate = navigateToCurrent;
+
+
+
+document.body.addEventListener('contextmenu', e => e.preventDefault() & e.stopPropagation());
+document.body.addEventListener('mousedown', onMouseDown);
+document.body.addEventListener('mouseup', onMouseUp);
+document.body.addEventListener('mousemove', onMouseMove);
+
+let showing, anchorX, anchorY, min = 100;
+const wheel = document.querySelector('.pie-wheel');
+
+function onMouseDown(ev) {
+    if (ev.button !== 2) return;
+    let { clientX: x, clientY: y } = ev;
+	showing = true;
+	anchorX = x;
+	anchorY = y;
+
+	wheel.style.setProperty('--x', `${x}px`);
+	wheel.style.setProperty('--y', `${y}px`);
+	wheel.classList.add('on');
+
+    document.querySelector('.pie-wheel').style.display = 'block';
+}
+
+function onMouseUp() {
+    showing = false;
+    document.querySelector('.pie-wheel').style.display = 'none';
+    wheel.classList.remove('on');
+    
+    const index = parseInt(wheel.getAttribute('data-chosen')) - 1;
+    if (!wheel.getAttribute('data-chosen') || index < 0) {
+        wheel.setAttribute('data-chosen', 0);
+        return;
+    }
+    
+    wheel.setAttribute('data-chosen', 0);
+    wheel.classList.remove('on');
+    
+    document.querySelector('.pie-wheel').style.display = 'none';
+    
+    navigateTo(navBarLabels[index].url);
+}
+
+function onMouseMove( ev ) {
+    let { clientX: x, clientY: y } = ev;
+	if (!showing) return;
+
+	let dx = x - anchorX;
+	let dy = y - anchorY;
+	let mag = Math.sqrt(dx * dx + dy * dy);
+	let index = 0;
+
+	if (mag >= min) {
+		let deg = Math.atan2(dy, dx) + 0.625 * Math.PI;
+		while (deg < 0) deg += Math.PI * 2;
+		index = Math.floor(deg / Math.PI * 4) + 1;
+	}
+
+	wheel.setAttribute('data-chosen', index);
+}
